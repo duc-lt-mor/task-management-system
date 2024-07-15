72636b7f865a7d1b7afb5e86f29f02bbda5b723f
fridayand13th <nguyentienmanh003@gmail.com>
2024-07-15 15:26:51 +0700

    config env

--- src/Config/config.ts
+++ src/Config/config.ts
@@ -1,11 +1,12 @@
 import { Sequelize } from "sequelize"
-
-const name = 'task_management_system'
-const user = `user`
-const password = `root`
+import * as dotenv from "dotenv"
+dotenv.config({path: __dirname+`.env`})
+const name : any = process.env.DB_NAME
+const user : any = process.env.USER
+const password : any  = process.env.PASSWORD
 
 export const sequelize = new Sequelize(name, user, password, {
-    host: `127.0.0.1`,
+    host: process.env.HOST ,
     dialect: `mysql`,
     port: 3305,
     define: {

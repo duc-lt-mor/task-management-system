1f4623d1c0d532332b1b9942690eaf73ffefa587
Do Trang Hieu <hieu.dt@mor.com.vn>
2024-07-24 17:13:37 +0700

    modified controller

--- src/Controller/UserController.ts
+++ src/Controller/UserController.ts
@@ -3,6 +3,7 @@ import { User } from '../Models/user';
 import bcrypt from 'bcrypt';
 import { generateToken } from '../Middleware/UserAuthenticator';
 import * as services from '../Services/UserServices';
+import { validationResult } from 'express-validator';
 
 export const getLogin = async function (
   req: express.Request,
@@ -36,7 +37,7 @@ export const getLogin = async function (
 
     return res.status(200).json({
       message: 'Login successful',
-      token
+      token,
     });
   } catch (err) {
     res.status(500).json(`Internal server error`);
@@ -48,14 +49,48 @@ export const postRegister = async function (
   res: express.Response,
 ) {
   try {
-    const result = await services.register(req.body);
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const data = {
+      name: req.body.name,
+      email: req.body.email,
+      password: req.body.password,
+      passwordConfirm: req.body.passwordConfirm,
+      phoneNumber: req.body.phone_number,
+      systemRoleID: req.body.system_role_id,
+    };
+
+    const existedUser = await User.findOne({
+      where: { email: data.email },
+    });
+
+    if (existedUser) {
+      return res.status(401).json({ message: `Email is already taken` });
+    }
+
+    const result = await services.register(data);
+
     if (result) {
       res.status(201).json({ message: 'User created', user: result });
     }
-    res.status(201).json({message: `User created`})
   } catch (err) {
-    res.status(500).json({message:`Internal server error`});
+    res.status(500).json({ message: `Internal server error`, err });
   }
 };
 
-
+export const deleteUser = async function (
+  req: express.Request,
+  res: express.Response,
+) {
+  try {
+    const name = req.body.name;
+    console.log(name);
+    const deleted = await services.deleteUser(name);
+    if (deleted) return res.status(200).json({ message: `Deleted user` });
+  } catch (err) {
+    return res.status(500).json(err);
+  }
+};

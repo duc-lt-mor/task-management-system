5ebb8443150a7464fa36c8530e149a82d5956492
Do Trang Hieu <hieu.dt@mor.com.vn>
2024-07-24 10:08:34 +0700

    add user interfaces

---
+++ src/Services/UserInterfaces.ts
@@ -0,0 +1,14 @@
+export interface UserPayload {
+  name: string;
+  email: string;
+  role: number;
+}
+
+export interface UserData {
+  name: string;
+  email: string;
+  password: string;
+  passwordConfirm: string;
+  phone_number: string;
+  system_role_id: string;
+}

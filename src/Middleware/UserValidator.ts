fc11b6af48d2695ade1528a2a5f69c8b6c82c9b2
Do Trang Hieu <hieu.dt@mor.com.vn>
2024-07-24 10:14:41 +0700

    added user validator

---
+++ src/Middleware/UserValidator.ts
@@ -0,0 +1,20 @@
+import { body } from 'express-validator';
+
+export const validator = function () {
+  return [
+    body(`name`).notEmpty().withMessage(`Please enter your name`),
+    body(`email`).notEmpty().withMessage(`Please enter your email`),
+    body(`password`).notEmpty().withMessage(`Please enter your password`),
+    body(`password_confirm`)
+      .notEmpty()
+      .withMessage(`Please enter your confirmation password`)
+      .custom((value, { req }) => value === req.body.password)
+      .withMessage('Password and confirmation password must match'),
+    body('phone_number')
+      .notEmpty()
+      .withMessage('Please fill in the right number format'),
+    body('system_role_id')
+      .notEmpty()
+      .withMessage('Please select a system role'),
+  ];
+};

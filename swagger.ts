727ab200b39db6b6fa32ea357bcd30d65d67fc68
Do Trang Hieu <hieu.dt@mor.com.vn>
2024-07-29 08:47:27 +0700

    config swagger

---
+++ swagger.ts
@@ -0,0 +1,15 @@
+import swaggerAutogen from "swagger-autogen";
+
+const doc = {
+    info: {
+      title: 'My API',
+      description: 'Description',
+    },
+    host: 'localhost:3000',
+    schemes: ['http'],
+  };
+  const outputFile = '../../swagger-output.json';
+  const endpointsFiles = ['./src/app.ts'];
+  swaggerAutogen()(outputFile, endpointsFiles, doc).then(() => {
+    console.log('Swagger documentation generated.');
+  });

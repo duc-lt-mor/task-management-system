13a19c9bff44e8c52284a11228645fae9cea69d7
Do Trang Hieu <hieu.dt@mor.com.vn>
2024-07-29 09:15:33 +0700

    update config

--- src/Routers/routers.ts
+++ src/Routers/routers.ts
@@ -1,5 +1,4 @@
 import * as user from "../Controller/UserController"
-import * as task from "../Controller/TaskController"
 import * as validator from "../Middleware/UserValidator"
 import express from "express"
 const router = express.Router()
@@ -9,12 +8,5 @@ import { exceptionHandler } from "../Middleware/ExceptionHandler"
 router.get('/login', user.getLogin)
 router.post('/register', ...validator.validateRegister(), user.postRegister)
 router.delete('/user/:userId', user.deleteUser)
-
-router.post('/task', authenticator.authenticateJWT, authenticator.accessControl('create_task'), task.generateTask)
-router.get('/task/:id', task.getTask)
-router.get('/task', task.getTasks)
-router.delete('/task/:id', task.deleteTask)
-
-router.put('/task', task.update)
 
 export { router }

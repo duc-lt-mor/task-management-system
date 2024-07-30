a9eb614913d5e54f7da6cdeef37c159f395baca9
Do Trang Hieu <hieu.dt@mor.com.vn>
2024-07-30 15:12:59 +0700

    relocate swagger and update routers

--- src/Routers/routers.ts
+++ src/Routers/routers.ts
@@ -2,19 +2,18 @@ import express from "express"
 import * as user from "../Controller/UserController"
 import * as task from "../Controller/TaskController"
 import * as authenticator from "../Middleware/UserAuthenticator"
-import { exceptionHandler } from "../Middleware/ExceptionHandler"
 import { validateTask }   from "../Middleware/TaskValidator"
 import * as userValidator from "../Middleware/UserValidator"
 
 const router = express.Router()
 
 router.get('/login', user.getLogin)
-router.post('/register', ...userValidator.validateRegister(), exceptionHandler, user.postRegister)
+router.post('/register', ...userValidator.validateRegister(), user.postRegister)
 router.delete('/user/:userId', user.deleteUser)
 
 router.post('/task', authenticator.authenticateJWT, authenticator.accessControl('create_task'), validateTask, task.generateTask)
-router.get('/task/:id', task.getTask)
-router.get('/task', task.getTasks)
+router.get('/task/:id', authenticator.authenticateJWT, authenticator.accessControl('get_task'), task.getTask)
+router.get('/task', authenticator.authenticateJWT, authenticator.accessControl('get_tasks'), task.getTasks)
 router.delete('/task/:id', authenticator.authenticateJWT, authenticator.accessControl('delete_task'), task.deleteTask)
 router.put('/task', task.update)
 

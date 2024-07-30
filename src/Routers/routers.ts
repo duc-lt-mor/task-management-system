c763bf7489d2d7b2a280aac1c89bf2a6e5cb4613
Do Trang Hieu <hieu.dt@mor.com.vn>
2024-07-30 08:56:17 +0700

    update task routers

--- src/Routers/routers.ts
+++ src/Routers/routers.ts
@@ -1,20 +1,21 @@
+import express from "express"
 import * as user from "../Controller/UserController"
 import * as task from "../Controller/TaskController"
-import * as validator from "../Middleware/UserValidator"
-import express from "express"
-const router = express.Router()
+import * as userValidator from "../Middleware/UserValidator"
 import * as authenticator from "../Middleware/UserAuthenticator"
 import { exceptionHandler } from "../Middleware/ExceptionHandler"
+import { validateTask }   from "../Middleware/TaskValidator"
+
+const router = express.Router()
 
 router.get('/login', user.getLogin)
-router.post('/register', ...validator.validateRegister(), exceptionHandler, user.postRegister)
+router.post('/register', ...userValidator.validateRegister(), exceptionHandler, user.postRegister)
 router.delete('/user/:userId', user.deleteUser)
 
-router.post('/task', authenticator.authenticateJWT, authenticator.accessControl('create_task'), task.generateTask)
+router.post('/task', authenticator.authenticateJWT, authenticator.accessControl('create_task'), validateTask, task.generateTask)
 router.get('/task/:id', task.getTask)
 router.get('/task', task.getTasks)
-router.delete('/task/:id', task.deleteTask)
-
+router.delete('/task/:id', authenticator.authenticateJWT, authenticator.accessControl('delete_task'), task.deleteTask)
 router.put('/task', task.update)
 
 export { router }

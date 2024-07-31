4790acf069c5eeec5a2cd6f23ab7fef53281c2e8
fridayand13th <nguyentienmanh003@gmail.com>
2024-07-31 09:31:44 +0700

    sua phan quyen

--- src/Middleware/CommentAuthenticator.ts
+++ src/Middleware/CommentAuthenticator.ts
@@ -5,62 +5,16 @@ import * as Role from '../Constant/Roles';
 import { System_role } from '../Models/system_role';
 import { Project_role } from '../Models/project_role';
 import { Task } from '../Models/task';
+import { Comment } from '../Models/comment';
 
-export const authenticateCDTask = function (permission: number) {
+export const authenticateCreateComment = function (permission: number) {
   return async (
     req: CustomRequest,
     res: express.Response,
     next: express.NextFunction,
   ) => {
     try {
-      if (!req.user) {
-        return res.status(401).json({ message: 'User not authenticated' });
-      }
-
-      let member: any = await Member.findOne({
-        where: {
-          user_id: req.user.id,
-          project_id:
-            Number(req.params.project_id) || Number(req.body.project_id),
-        },
-        include: [
-          {
-            model: Project_role,
-          },
-        ],
-      });
-
-      let system_role: any = await System_role.findOne({
-        where: {
-          id: req.user.system_role_id,
-        },
-      });
-
-      if (system_role.key == Role.ADMIN) {
-        next();
-      } else if (
-        member?.project_role.key == Role.USER ||
-        !member?.project_role.permission_keys.permissions.includes(permission)
-      ) {
-        return res
-          .status(403)
-          .json({ message: 'You do not have permission to access.' });
-      } else {
-        next();
-      }
-    } catch (err) {
-      return res.status(500).json({ message: 'Internal error ' });
-    }
-  };
-};
-
-export const authenticateUpdateTask = function (permission: number) {
-  return async (
-    req: CustomRequest,
-    res: express.Response,
-    next: express.NextFunction,
-  ) => {
-    try {
+        
       if (!req.user) {
         return res.status(401).json({ message: 'User not authenticated' });
       }
@@ -86,27 +40,101 @@ export const authenticateUpdateTask = function (permission: number) {
 
       let task: any = Task.findOne({
         where: {
-          id: req.params.task_id,
+          id: req.body.task_id,
         },
       });
 
-      await Promise.all([member,system_role,task]);
+      await Promise.all([member, system_role, task]);
 
       if (system_role.key == Role.ADMIN) {
         next();
       } 
-
-      else if (
-        member?.project_role.key != Role.USER &&
-        member?.project_role.permission_keys.permissions.includes(permission)
-      ) {
+      
+      else if (member?.project_role.key != Role.USER) {
         next();
       } 
-
+      
       else if (task.assignee_id == req.user.id) {
         next();
       } 
+      
+      else {
+        return res
+          .status(403)
+          .json({ message: 'You do not have permission to access.' });
+      }
+    } catch (err) {
+      return res.status(500).json({ message: 'Internal error ' });
+    }
+  };
+};
 
+export const authenticateUpdateComment = function (permission: number) {
+  return async (
+    req: CustomRequest,
+    res: express.Response,
+    next: express.NextFunction,
+  ) => {
+    try {
+
+      if (!req.user) {
+        return res.status(401).json({ message: 'User not authenticated' });
+      }
+
+      let comment: any = await Comment.findOne({
+        where: {
+          id: req.params.comment_id,
+        },
+      });
+
+      if (comment.user_id == req.user.id) {
+        next();
+      } 
+      
+      else {
+        return res
+          .status(403)
+          .json({ message: 'You do not have permission to access.' });
+      }
+    } catch (err) {
+      return res.status(500).json({ message: 'Internal error ' });
+    }
+  };
+};
+
+export const authenticateDeleteComment = function (permission: number) {
+  return async (
+    req: CustomRequest,
+    res: express.Response,
+    next: express.NextFunction,
+  ) => {
+    try {
+      if (!req.user) {
+        return res.status(401).json({ message: 'User not authenticated' });
+      }
+
+      let system_role: any = System_role.findOne({
+        where: {
+          id: req.user.system_role_id,
+        },
+      });
+
+      let comment: any = Comment.findOne({
+        where: {
+          id: req.body.task_id,
+        },
+      });
+
+      await Promise.all([system_role, comment]);
+
+      if (system_role.key == Role.ADMIN) {
+        next();
+      } 
+      
+      else if (comment.user_id == req.user.id) {
+        next();
+      } 
+      
       else {
         return res
           .status(403)

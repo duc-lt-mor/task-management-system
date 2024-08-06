619cd239a8a0bd8135a36509994df20eb9591029
Do Trang Hieu <hieu.dt@mor.com.vn>
2024-08-06 15:29:34 +0700

    update statistics

--- src/Services/StatisticServices.ts
+++ src/Services/StatisticServices.ts
@@ -11,22 +11,47 @@ export const totalTask = async function (id: number) {
   return tasks;
 };
 
-export const countOnDateTask = async function (id: number) {
-  let tasks: number = await Task.count({
-    where: Sequelize.literal(
-      'real_end_date <= expected_end_date AND project_id = ' + id,
-    ),
-  });
 
-  return tasks;
-};
 
 export const showFinishOnDateTask = async function (id: number) {
   let onDateTask: any = await Task.findAll({
-    where: Sequelize.literal(
-      'real_end_date <= expected_end_date AND project_id = ' + id,
-    ),
+    where: {
+      expected_end_date: {
+        [Op.gte]: `real_end_date`
+      },
+      project_id: id
+    },
   });
 
   return onDateTask;
 };
+
+export const showUnfinishedTask = async function (id: number) {
+  const unfinishedTasks = await Task.findAll({
+    where: {
+      real_end_date: null
+    }
+  })
+  if (unfinishedTasks.length === 0) {
+    return `Project ${id} has no unfinished tasks.`;
+  } else {
+    return `Project ${id} has unfinished tasks: ${JSON.stringify(unfinishedTasks, null, 2)}`;
+  }
+}
+
+export const showFinishBehindDateTask = async function (id: number) {
+  const lateTasks: any = await Task.findAll({
+    where: {
+      real_end_date: {
+        [Op.gt]: Sequelize.col(`expected_end_date`)
+      },
+      project_id: id,
+    }
+  });
+  if (lateTasks.length === 0) {
+    return `Project ${id} has no tasks that finished behind schedule.`;
+  } else {
+    return `Project ${id} has tasks that finished behind schedule: ${JSON.stringify(lateTasks, null, 2)}`;
+  }
+};
+

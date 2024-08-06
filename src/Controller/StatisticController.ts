6d1a1bf4a40dde23addbd51b833b078af77f8709
Do Trang Hieu <hieu.dt@mor.com.vn>
2024-08-06 15:54:44 +0700

    update statistics

--- src/Controller/StatisticController.ts
+++ src/Controller/StatisticController.ts
@@ -25,7 +25,7 @@ export const showFinishOnDateTask = async function (
       Number(req.params.project_id),
     );
     let total_task = await Statistic.totalTask(Number(req.params.project_id));
-    let total_ondate_task = await Statistic.countOnDateTask(
+    let total_ondate_task = await Statistic.showFinishOnDateTask(
       Number(req.params.project_id),
     );
     let b: number = (total_ondate_task / total_task) * 100;
@@ -39,3 +39,33 @@ export const showFinishOnDateTask = async function (
     next(error);
   }
 };
+
+export const showUnfinishedTask = async function (
+  req: express.Request,
+  res: express.Response,
+  next: express.NextFunction
+) {
+  try {
+    const unfinished = await Statistic.showUnfinishedTask(Number(req.params.id))
+    if (unfinished) {
+      return res.status(200).json(unfinished)
+    }
+  } catch (err) {
+    next(err)
+  }
+}
+
+export const showBehindDateTask = async function (
+  req: express.Request,
+  res: express.Response,
+  next: express.NextFunction
+) {
+  try {
+    const lateTask = await Statistic.showFinishBehindDateTask(Number(req.params.id))
+    if (lateTask) {
+      return res.status(200).json(lateTask)
+    }
+  } catch (err) {
+    next(err)
+  }
+}

4a98b80e3a243457cc40d139675b7c64d8e23abd
Do Trang Hieu <hieu.dt@mor.com.vn>
2024-08-07 13:28:59 +0700

    added date validator to ensure logical flow

--- src/Middleware/TaskValidator.ts
+++ src/Middleware/TaskValidator.ts
@@ -5,9 +5,20 @@ export const validateTask = function () {
     body(`name`).notEmpty().withMessage(`Please enter task name`),
     body(`description`).notEmpty().withMessage(`Please enter task description`),
     body(`priority`).notEmpty().withMessage(`Please insert task priority`),
-    body(`expected_end_date`)
+    body('start_date').notEmpty().withMessage('Please enter the start date'),
+    body('expected_end_date')
       .notEmpty()
-      .withMessage(`Please enter desired completion date`),
+      .withMessage('Please enter the desired completion date')
+      .custom((value, { req }) => {
+        const start_date = new Date(req.body.start_date);
+        const end_date = new Date(value);
+        if (start_date >= end_date) {
+          throw new Error(
+            'Expected end date must be greater than the start date',
+          );
+        }
+        return true;
+      }),
     body('assignee_id')
       .notEmpty()
       .withMessage('Please select an employee to complete the task'),
